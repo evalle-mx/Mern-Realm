d49ebc6843abcd8ab17a46439b18ecf75fe7ee57
Ernesto Valle <evalle.mx@gmail.com>
2023-02-03 22:50:45 -0500

    Part1, connecting to DB and show list OK

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import app from './server.js';
+import mongodb from 'mongodb';
+import dotenv from 'dotenv';
+import RestaurantsDAO from './dao/restaurantsDAO.js';
+
+dotenv.config();
+const MongoCliente = mongodb.MongoClient;
+
+const port = process.env.PORT || 8000;
+
+MongoCliente.connect(
+    process.env.RESTREVIEWS_DB_URI, 
+    {
+        maxPoolSize: 50,
+        wtimeoutMS: 2500
+    }
+).catch(err => {
+    console.error(err.stack);
+    process.exit(1);
+}).then( async client => {
+    await RestaurantsDAO.injectDB(client)
+    app.listen(port, () => {
+        console.log(`listening on por ${port}`);
+    })
+})
+;
+
+
+/* 
+Deprecated:
+ poolSize: 50, wtimeout: 2500, useNewUrlParse: true
+*/
